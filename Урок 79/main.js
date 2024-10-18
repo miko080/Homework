59545e0c25e96ba52b629bdb5cd037fcaf8c39bb
miko080 <s.zhanar173@gmail.com>
2024-10-18 16:31:03 +0500

    Add files via upload

---
+++ Урок 79/main.js
@@ -0,0 +1,39 @@
+// const firstNumber = 10
+// const secondNumber = 20
+// if (firstNumber === 10 && secondNumber === 20) {
+//     result = firstNumber + secondNumber
+//     console.log(result);
+// } else {
+
+//     result = firstNumber - secondNumber
+//     console.log(result);
+// }
+
+// ============================================================
+
+// const alex = 'admin'
+// if(alex === 'user') {
+//     console.log('Alex is user');
+    
+// } else if (alex === 'admin') {
+//     console.log('Alex is admin');
+// }
+
+// ================================================================
+// const alex = 'admin'
+// const alex = 'admin' ? console.log('Alex is admin') : console.log('Alex is not admin');
+
+// ==============================================================================
+
+const age = 16
+if( age >= 18 ) {
+    console.log('Вы совершеннолетний');
+} else {
+    console.log('Вы не совершеннолетний');
+}
+
+// =====================================================================
+
+
+const myAge = 16
+myAge === 18 ? console.log('Вы совершеннолетний') : console.log('Вы не совершеннолетний');
